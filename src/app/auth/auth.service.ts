55aa2f618332bb10ef0c3d634c41310ae9018eeb
Archisman Tripathi <archismantripathi@gmail.com>
2024-04-24 10:28:18 +0530

    fix login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,40 +1,48 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { uri } from '../config/uri.config';
+import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  login(username: string, password: string): boolean {
-    let noErr: boolean = false
-    this.http
+  login(username: string, password: string): Observable<boolean> {
+    return this.http
       .post<string>(uri + 'api/user/login', {
         username: username,
         password: password,
       })
-      .subscribe((data: any) => {
-        if (data != null) {
-          localStorage.setItem('Authorization', data.token);
-          this.router.navigate(['/todo']);
-          noErr=true;
-        }
-      });
-    return noErr;
+      .pipe(
+        map((data: any) => {
+          if (data != null) {
+            localStorage.setItem('Authorization', data.message);
+            this.router.navigate(['/todo']);
+            return true;
+          } else {
+            return false;
+          }
+        }),
+        catchError((error: any) => {
+          let errorMessage = 'Something went wrong';
+          if (error.error && error.error.message) {
+            errorMessage = error.error.message.substr(7);
+          }
+          console.log(errorMessage);
+          return throwError(() => errorMessage);
+        })
+      );
   }
 
-  logout() {
+  logout(): void {
     localStorage.clear();
     this.router.navigate(['/login']);
   }
 
-  isAuth() {
+  isAuth(): boolean {
     if (localStorage.getItem('Authorization')) {
       return true;
     } else {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import {
   MatSnackBar,
-  MatSnackBarHorizontalPosition
+  MatSnackBarHorizontalPosition,
 } from '@angular/material/snack-bar';
 
 @Component({
@@ -23,39 +28,53 @@ import {
     MatInputModule,
     RouterLink,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private _snackBar: MatSnackBar){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   loginForm = this.formBuilder.group({
-    username: ['', [
-      Validators.required,
-      Validators.nullValidator
-    ]],
-    password: ['',[
-      Validators.required,
-      Validators.minLength(8)
-    ]]
+    username: ['', [Validators.required, Validators.nullValidator]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
   });
-  get form() { return this.loginForm.controls; };
-  onSubmit() {
-    if(this.loginForm.invalid) {
-      this.openSnackBar("Invalid Form");
+
+  get form() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() :void {
+    if (this.loginForm.invalid) {
+      this.openSnackBar('Invalid Form');
       return;
     }
-
-    if(!this.authService.login(String(this.loginForm.value.username),String(this.loginForm.value.password)))
-      this.openSnackBar("Wrong credentials");
+    this.authService
+      .login(
+        String(this.loginForm.value.username),
+        String(this.loginForm.value.password)
+      )
+      .subscribe({
+        next: (success: boolean) => {
+          if (success) this.openSnackBar("Welcome, 'x'");
+          else this.openSnackBar('Something went wrong');
+        },
+        error: (errorMessage: string) => {
+          this.openSnackBar(errorMessage);
+        },
+      });
+    return;
   }
 
   openSnackBar(msg: string): void {
     this._snackBar.open(msg, 'Got it!', {
-      horizontalPosition: this.horizontalPosition
-  });}
+      horizontalPosition: this.horizontalPosition,
+    });
+  }
 }
